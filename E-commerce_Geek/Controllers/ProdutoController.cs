d8be1afe3dcab8de6876af767147679e4e9dc715
Kevin Bandeira do Nascimento <kevinbandeira2006@gmail.com>
2025-01-04 01:01:12 -0300

    trazendo primeira tela do vscode e montando estrutura básica para o asp net

---
+++ E-commerce_Geek/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce_Geek.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Produto()
+        {
+            return View();
+        }
+
+        public IActionResult Favoritos()
+        {
+            return View();
+        }
+    }
+}
---
+++ E-commerce_Geek/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce_Geek.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+    }
+}
